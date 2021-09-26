6e8037afe4e7526cb83bf2a94339668755eef64c
Dexter <adnan.ch09@protonmail.com>
2021-09-26 16:03:23 +0530

    Updated background.js

--- background.js
+++ background.js
@@ -5,28 +5,28 @@ const slider = document.getElementById("slider");
 const value = document.getElementById("value");
 const doneButton = document.getElementById("done-button");
 
-value.innerHTML = "Time in minutes: " + slider.value;
+value.innerHTML = "Time: " + slider.value + " minutes";
 
 slider.oninput = (e) => {
-    value.innerHTML = "Time in minutes: " + e.target.value;
+    value.innerHTML = "Time: " + e.target.value + " minutes";
 };
 
 doneButton.onclick = (e) => {
     reloadInterval = slider.value * 60 * 1000;
     if (slider.value == 1)
-        alert("Time set! You'll be notified with memes in a minute");
+        alert("Time set! You'll be updated with memes in a minute");
     else
         alert(
-            "Time set! You'll be notified with memes in " + slider.value + " minutes."
+            "Time set! You'll be updated with memes in " + slider.value + " minutes."
         );
     window.close();
 };
 
-function xyz() {
+function window_opener() {
     chrome.windows.create({
         url: "memes.html",
         focused: true,
         type: "popup",
     });
 }
-setTimeout(xyz, reloadInterval);
+setTimeout(window_opener, 5000);
