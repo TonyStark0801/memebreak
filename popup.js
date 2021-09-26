01a0e7792d76bb9012f0e45f9eb16359bbb56742
Dexter <adnan.ch09@protonmail.com>
2021-09-26 15:18:18 +0530

    Deleted jQuery file

--- popup.js
+++ popup.js
@@ -1,10 +1,19 @@
-const subs = ["dankmemes", "memes", "me_irl", "cursedcomments", "wholesomememes", "funny", "gifs", "hmmm"];
+const subs = [
+    "dankmemes",
+    "memes",
+    "me_irl",
+    "cursedcomments",
+    "wholesomememes",
+    "funny",
+    "gifs",
+    "hmmm",
+];
 
 var item = subs[Math.floor(Math.random() * subs.length)];
 
 var url;
 
 fetch("https://meme-api.herokuapp.com/gimme/" + item)
-    .then(res => res.json())
-    .then(data => url = data['url'])
-    .then(() => document.getElementById("img").src = url);
+    .then((res) => res.json())
+    .then((data) => (url = data["url"]))
+    .then(() => (document.getElementById("img").src = url));
