83f46d87015992b26d33631242e1d4698629ae09
Dexter <adnan.ch09@protonmail.com>
2021-09-26 15:14:12 +0530

    Updated background.js to update var url;

---
+++ background.js
@@ -0,0 +1,32 @@
+var url;
+var reloadInterval;
+
+const slider = document.getElementById("slider");
+const value = document.getElementById("value");
+const doneButton = document.getElementById("done-button");
+
+value.innerHTML = "Time in minutes: " + slider.value;
+
+slider.oninput = (e) => {
+    value.innerHTML = "Time in minutes: " + e.target.value;
+};
+
+doneButton.onclick = (e) => {
+    reloadInterval = slider.value * 60 * 100;
+    if (slider.value == 1)
+        alert("Time set! You'll be notified with memes in a minute");
+    else
+        alert(
+            "Time set! You'll be notified with memes in " + slider.value + " minutes."
+        );
+    window.close();
+};
+
+function xyz() {
+    chrome.windows.create({
+        url: "memes.html",
+        focused: true,
+        type: "popup",
+    });
+}
+setTimeout(xyz, 1000);
