fae465c975786c55231745786f95628d31cfb109
Shubham Mishra <mishrashubham2001@gmail.com>
2022-06-10 02:11:25 +0530

    background.js file added to run timer in background

--- src/js/background.js
+++ src/js/background.js
@@ -1,34 +1,10 @@
-const value = document.getElementById("value");
-const slider = document.getElementById("slider");
-const doneButton = document.getElementById("done-button");
-var y;
-
-// value.innerHTML = "Time: " + slider.value + " minutes";
-
-slider.oninput = (e) => {
-    value.innerHTML = "Time: " + e.target.value + " minutes";
-};
-
-doneButton.onclick = (e) => {
-    y = slider.value ;
-    // if (slider.value == 1)
-    alert(`Time set! You'll be updated with memes in ${y} minute`);
-    // else alert("Time set! You'll be updated with memes in " + y + " minutes.");
-    timeout();
-
-    function timeout() {
-        setTimeout(function() {
-            windowOpener();
-        }, y);
-        clearInterval();
-    }
-    // window.close();
-};
-
-function windowOpener() {
-    chrome.windows.create({
-        url: "memes.html",
-        focused: true,
-        type: "popup",
+    
+    chrome.alarms.onAlarm.addListener(function() {
+        chrome.windows.create({
+            url: "../../src/html/memes.html",
+            focused: true,
+            type: "popup",
+            height:550,
+            width:750
+        });
     });
-}
